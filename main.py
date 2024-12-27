9cebb14dea8484ad3f448fea719f4f284e52d089
maria <mariaguerries@gmail.com>
2024-12-26 22:56:59 -0500

    Created main.py and censored server details.

---
+++ main.py
@@ -0,0 +1,13 @@
+# Making a program that is connected to a Postgres SQL Server where the user is able to insert data into the server with their input.
+# Date: December 26, 2024
+# MacOS Sonoma / Python 3.12
+# main.py
+
+# Importing the module that connects the Postgres SQL Server to Python.
+
+
+#############################
+#                           #
+#      SERVER DETAILS       #
+#                           #
+#############################
